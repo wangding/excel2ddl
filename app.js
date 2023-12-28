17c2b952a828d21521edee39490dac69c54b4aff
wangding <408542507@qq.com>
2023-12-28 13:17:33 +0800

    splite modules

--- app.js
+++ app.js
@@ -1,86 +1,25 @@
 #!/usr/bin/env node
 
-const log  = console.log,
-      path = require('path'),
-      fs   = require('fs'),
-      xls  = require('xlsx');
-
-// input:  excel file with xlsx extension
-// output: metaData
-function metaData(file) {
-  const workbook = xls.readFile(file);
-  const arr2obj = ([tableName, sheet]) => ({
-    tableName,
-    fields: xls.utils.sheet_to_json(sheet)
-  });
-  return {
-    dbName: path.basename(file, '.xlsx'),
-    tables: Object.entries(workbook.Sheets).map(arr2obj),
-  };
-}
-
-const ddlCreateDB = dbName => '' +
-  `drop database if exists ${dbName};\n` +
-  `create database ${dbName} default character set utf8mb4;\n` +
-  `\nuse ${dbName};\n\n`;
-
-function columnDefinition(field) {
-  const primaryKey = () => (field.Key === 'PK') ? 'primary key' : '';
-  const autoIncrement = () => (field.Field.includes('id') && field.Key === 'PK') ? 'auto_increment ' : '';
-  function defaultValue(arg) {
-    if(typeof arg === 'undefined') return '';
-    if(arg.toLowerCase() === 'null') return `default ${arg}`;
-    if(typeof Number(arg) === 'number' && !isNaN(Number(arg))) return `default ${arg}`;
-
-    return `default '${arg}'`;
-  }
-
-  return '  ' +
-    (`${field.Field} ` +
-    `${field.Type} ` +
-    `${field.Null == 'No' ? 'not null ': ''}` +
-    defaultValue(field.Default) +
-    autoIncrement() +
-    primaryKey()).trim();
-}
-
-const ddlCreateTB = table => '' +
-  `drop table if exists ${table.tableName};\n` +
-  `create table ${table.tableName} (\n` +
-  table.fields.map(columnDefinition).join(',\n') +
-  `\n) engine=innodb default charset=utf8mb4;\n`;
-
-const value = sample => (field) => field.Type.includes('char')
-  ? `"${field[sample]}"` : field[sample];
-
-// input: table
-// output: SQL DML insert statements
-const dmlInsert = table => '' +
-  `insert into ${table.tableName} (` +
-  table.fields.map(e => e.Field).join(', ').trim() +
-  ') values (' +
-  table.fields.map(value('Sample1')).join(', ').trim() +
-  '), (' +
-  table.fields.map(value('Sample2')).join(', ').trim() + ');'
-
-// input: metaData
-// output: SQL DDL statements
-const genDDL = metaData => '' +
-  ddlCreateDB(metaData.dbName) +
-  metaData.tables.map(ddlCreateTB).join('\n') + '\n' +
-  metaData.tables.map(dmlInsert).join('\n');
+const commands = require('./lib/cmds');
+const { metaData } = require('./lib/metadata');
 
 function main() {
   const argc = process.argv.length,
-        file = process.argv[2];
+        cmd  = process.argv[2],
+        file = process.argv[3];
 
-  if(argc !== 3) {
-    log('Usage: cmd database-name.xls');
+  if(argc !== 4) {
+    console.log('Usage: cmd subcmd database-name.xls');
+    return;
+  }
+
+  if(!Reflect.has(commands, cmd)) {
+    console.log(`${cmd} not support!`);
     return;
   }
 
   const data = metaData(file);
-  fs.writeFileSync(`./${data.dbName}.sql`, genDDL(data));
+  commands[cmd](data);
 }
 
 main();
